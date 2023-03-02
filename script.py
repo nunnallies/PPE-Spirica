562dc1144c81ff2d0c81bcbaed2185fcec99d592
Lydiaa21 <lydiaclo21@gmail.com>
2023-03-02 19:43:07 +0100

    script python pour recuperer num SIREN

---
+++ script.py
@@ -0,0 +1,26 @@
+import PyPDF2
+
+pdf_file = open('Users/Lydia/LOREAL.pdf', 'rb')
+
+if pdf_file :
+    print("Doc ouvert")
+else :
+    print("doc pas ouvert")
+
+pdf_reader = PyPDF2.PdfReader(pdf_file)
+pdf_page_count = len(pdf_reader.pages)
+print(pdf_page_count)
+
+text = ''
+for page in range(pdf_page_count):
+    page_obj = pdf_reader.pages[page]
+    text += page_obj.extract_text()
+
+print(text)
+siren = ''
+# Chercher le numéro SIREN
+for line in text.split('\n'):
+    if 'SIREN' in line:
+        siren = line.split('SIREN')[1].strip().replace(' ', '')
+
+print(siren)
