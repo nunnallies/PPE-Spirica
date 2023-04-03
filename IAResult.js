3c0ceb7152c0c9430bedf7cc748dd117854b6dcb
FrancisBbrt <101263305+FrancisBbrt@users.noreply.github.com>
2023-04-03 23:28:58 +0200

    Modif : Resultat + Mes Contrats

--- IAResult.js
+++ IAResult.js
@@ -1,29 +1,11 @@
-const buffering = () => {
-  const bar = document.querySelector(".loarder");
-  let width = 1;
-  var interval = 100,
-    remaining = interval / 10,
-    minus_rem = remaining / 100;
-
-  const rate = () => {
-    const d = new Date();
-    let seconds = d.getSeconds();
-
-    bar.style.width = `${width}%`;
-    document.querySelector(".percentage").innerHTML = `${width}%`;
-
-    remaining = remaining - minus_rem;
-
-    if (width >= 100) {
-      remaining = 0;
-      document.querySelector(".done").style = "display:inline-block";
-      clearInterval(interval);
-    }
-    document.querySelector(".seconds").innerHTML = Math.ceil(remaining);
-    width++;
-  };
-  const intervale = setInterval(rate, interval);
-};
-document
-  .querySelector("progress_result_button")
-  .addEventListener("click, buffering");
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+  button.classList.add("active");
+  setTimeout(() => {
+    button.classList.remove("active");
+    button
+      .querySelector("i")
+      .classList.replace("bx-cloud-download", "bx-check-circle");
+    button.querySelector("span").innerText = "Completed";
+  }, 6000);
+});
